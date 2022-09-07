f9becaa1fa3d9dd3380dec091795534453ddd21a
Alejandro Hugo Herms <hermsale@gmail.com>
2022-09-07 10:36:10 -0300

    se utilizo la logica async await y try and catch para resolver las llamadas a la API y resolver distintas peticiones

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -8,30 +8,17 @@ async function fetchData(urlApi){
     return data;
 }
 
-<<<<<<< HEAD
 // declaramos una funcion anonima async que en sus parametros tenga la url 
 const anotherfunction = async (urlApi) => {
     // en try sucede lo que queremos que forma parte de la logia de nuestra app - es lo que llega en el resolve 
-=======
- 
-// declaramos una funcion anonima async que en sus parametros tenga la url
-const anotherfunction = async (urlApi) => {
-    // en try sucede lo que queremos que forma parte de la logia de nuestra app - es lo que llega en el resolve
->>>>>>> d62b1a6378f94547e51fb451627559372cc4946e
     try{
         const products = await fetchData(`${urlApi}/products`); // hacemos una solicitud a la api, y guardamos en products la data (todos los products)
         const product = await fetchData(`${urlApi}/products/${products[0].id}`); // accedemos al primer producto del arreglo y guardamos su id
         const category = await fetchData(`${urlApi}/categories/${product.category.id}`) // utilizamos lo llegado en el segundo llamado, para acceder a category y su id
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> d62b1a6378f94547e51fb451627559372cc4946e
         console.log('todos los productos ', products);
         console.log('titulo de un producto ',product.title);
         console.log('categoria de un producto ',category.name);
     }
-<<<<<<< HEAD
     // entra lo que llegue en el reject 
     catch (error){
         console.error(error); // en caso que falle algo, caera en este recurso 
@@ -39,12 +26,3 @@ const anotherfunction = async (urlApi) => {
 }
 
 anotherfunction(API);
-=======
-    // entra lo que llegue en el reject
-    catch (error){
-        console.error(error); // en caso que falle algo, caera en este recurso
-    }
-}
- 
-anotherfunction(API);
->>>>>>> d62b1a6378f94547e51fb451627559372cc4946e
